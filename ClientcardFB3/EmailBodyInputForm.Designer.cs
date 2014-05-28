36a98d5dec6476fb58dbbe3dba84148cb4db7a6d
zenev <nevan.wichers@yahoo.com>
2014-05-27 17:55:23 -0700

    made the email form look how it should. Still has same functionality as before though

--- ClientcardFB3/EmailBodyInputForm.Designer.cs
+++ ClientcardFB3/EmailBodyInputForm.Designer.cs
@@ -31,45 +31,123 @@
             this.tbEmailBody = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.tbSubject = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tbEmailBody
             // 
-            this.tbEmailBody.Location = new System.Drawing.Point(8, 48);
+            this.tbEmailBody.Location = new System.Drawing.Point(15, 179);
+            this.tbEmailBody.Margin = new System.Windows.Forms.Padding(4);
             this.tbEmailBody.Multiline = true;
             this.tbEmailBody.Name = "tbEmailBody";
-            this.tbEmailBody.Size = new System.Drawing.Size(584, 296);
+            this.tbEmailBody.Size = new System.Drawing.Size(706, 360);
             this.tbEmailBody.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Location = new System.Drawing.Point(13, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(361, 20);
+            this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Please Input The Text For The Body Of The Email:";
+            this.label1.Text = "Email Body:";
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(512, 16);
+            this.btnOK.Location = new System.Drawing.Point(11, 13);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
             this.btnOK.TabIndex = 2;
-            this.btnOK.Text = "OK";
+            this.btnOK.Text = "Send";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(118, 12);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 29);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "To:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Subject:";
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(118, 45);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(603, 22);
+            this.tbTo.TabIndex = 7;
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(118, 73);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(603, 22);
+            this.tbFrom.TabIndex = 8;
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Location = new System.Drawing.Point(118, 101);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(603, 22);
+            this.tbSubject.TabIndex = 9;
+            // 
             // EmailBodyInputForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
-            this.ClientSize = new System.Drawing.Size(599, 352);
+            this.ClientSize = new System.Drawing.Size(736, 552);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.tbFrom);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbEmailBody);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "EmailBodyInputForm";
             this.Text = "Email Body Input Form";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EmailBodyInputForm_FormClosing);
@@ -83,5 +161,12 @@
         private System.Windows.Forms.TextBox tbEmailBody;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.TextBox tbSubject;
     }
 }
