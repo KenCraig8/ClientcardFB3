d7ee6d4fe982f77969a798e29699915391b273e3
zenev <nevan.wichers@yahoo.com>
2014-08-11 22:21:51 -0700

    added CustomSqlWhere

---
+++ CustomSql/CustomSql/DataHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MapData2
+{
+    public class DataHelper
+    {
+        public static DataTable sqlQuery(string connectionString, string sqlQuery)
+        {
+            SqlConnection databaseConnection = new SqlConnection(connectionString);
+            databaseConnection.Open();
+
+            SqlCommand dataCommand = new SqlCommand(sqlQuery, databaseConnection);
+
+            DataTable dTable = new DataTable();
+            SqlDataAdapter dadAdpt = new SqlDataAdapter();
+            dadAdpt.SelectCommand = dataCommand;
+            dadAdpt.Fill(dTable);
+
+            databaseConnection.Close();
+
+            return dTable;
+        }
+
+        //Returns all of the entrys in a one column DataTable as an array
+        public static string[] dataTableToArray(DataTable table)
+        {
+            return table.AsEnumerable().Select(row => Convert.ToString(row.Field<object>(0))).ToArray();
+        }
+
+        public static string enumToSqlIn(System.Collections.IEnumerable inEnum)
+        {
+            const string kSeperator = ", ";
+
+            string valuesStr = "";
+
+            //I can't use the String.Join for this because it will put the empty string in for null values
+            //so the resulting string will look like "[itemName, , , , , ]
+            foreach (object item in inEnum)
+            {
+                if (item != null)
+                    valuesStr += "'"+item+"'" + kSeperator;
+            }
+            
+            return "(" + valuesStr.Substring(0, valuesStr.Length - kSeperator.Length) + ")";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+ClientCard
+==========
+
+An application to allow food banks to store and view their data
+
+
+Getting started with the project
+
+Download these files:
+ClientCard install: FB3Installion-2014-04-27.zip
+
+Install Client Card:
+	Run .msi installer
+
+Install these:
+Microsoft Access 2010 or better
+SQL server 2008 R2 Express with tools: http://www.microsoft.com/en-us/download/details.aspx?id=30438
+The file SQLEXPRWT_x64_ENU.exe or SQLEXPRWT_x86_ENU.exe
+		Change these options to install. Everything else is the default:
+			Server configeration: Set Browser Service to Automatic.
+			Database Engine config: Mixed Mode Authentication with a SA password (I use ccfb3)
+
+Run SQL Management Studio
+LogIn to SQL Management Studio using Windows Authentication or the SA user
+	Right click on Database folder, Restore Database:
+		To database: ClientcardFB3
+		In General tab: select Source-> from device-> add->		C:\Users\Public\ClientcardFB3\Database\FB3x8Prod.BAK to database ClientCardFB3
+	Create New LogIn: Right click on Security folder, under server (not the one under databases) Select SQL server authentication. Username: CCFB_User pw: 19800612
+	Reset permissions:  Open file icon, open: C:\Users\Public\ClientcardFB3\Scripts\ResetCCFBUser.sql, click execute
+
+Run Client Card with login Admin, password: master
