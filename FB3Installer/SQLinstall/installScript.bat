7a55268bc174ee3570051acae47ea5407faf7632
zenev <nevan.wichers@gmail.com>
2014-09-12 19:54:01 -0700

    minor changes

--- FB3Installer/SQLinstall/installScript.bat
+++ FB3Installer/SQLinstall/installScript.bat
@@ -1,2 +1,7 @@
+echo "starting"
 :r ./RestoreDatabase.sql
+echo "restore done"
 :r ./SetUser.sql
+echo "set user done"
+:r "C:\Users\Public\ClientcardFB3\Scripts\ResetCCFBUser.sql"
+echo "reset user done"
